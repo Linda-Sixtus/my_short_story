2c60367ea1288b9d7af9a637fe6d3094c9d24bf7
Linda Sixtus <linda.sixtus@gmx.de>
2025-05-23 11:09:09 +0200

    neue sache zum ersten mal

---
+++ chapter_3/main.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  int gerry;
+  String fluxi = sayHello("Fluxi");
+
+  gerry=4;
+  gerry++;
+
+  print(fluxi);
+  print(gerry);
+
+}
+
+String sayHello(String name) {
+  return "Hallo " + name;
+}

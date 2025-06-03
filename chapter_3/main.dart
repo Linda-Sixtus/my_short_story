fdca0b453465c6585563210132833a65a9960263
Linda Sixtus <linda.sixtus@gmx.de>
2025-06-03 11:47:37 +0200

    Lösung_aufgabe_1-3.3.3

--- chapter_3/main.dart
+++ chapter_3/main.dart
@@ -1,8 +1,15 @@
 void main() {
-int year = 13;
-String city = "Bonn";
-double pi = 3.14;
-bool isActive = true;
-
   String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int  d = 20;
+  int seven = 7;
+  int twelve = 12;
+  int a = 24;
+  String dStr = "!";  // Korrigiert
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+  String timeStr = "Zeit";
 }

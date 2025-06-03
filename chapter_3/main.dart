b259e761ddbcfcbe5ef021262e97911289187fe0
Linda Sixtus <linda.sixtus@gmx.de>
2025-06-03 11:29:33 +0200

    main.dart.linda.first.today3.3.3

--- chapter_3/main.dart
+++ chapter_3/main.dart
@@ -1,16 +1,8 @@
 void main() {
+int year = 13;
+String city = "Bonn";
+double pi = 3.14;
+bool isActive = true;
 
-  int gerry;
-  String fluxi = sayHello("Fluxi");
-
-  gerry=4;
-  gerry++;
-
-  print(fluxi);
-  print(gerry);
-
-}
-
-String sayHello(String name) {
-  return "Hallo " + name;
+  String gdStr = "Guten";
 }
